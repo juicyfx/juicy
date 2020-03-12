662618746b2ce69ad98ccb29c25877fce4a46b79
Milan Felix Šulc <rkfelix@gmail.com>
2020-03-12 18:22:03 +0100

    [qrx] added links to examples

--- tools/qrx/api/_lib/errors.ts
+++ tools/qrx/api/_lib/errors.ts
@@ -2,11 +2,11 @@ export const USAGE = `
     <h1>QRX</h1>
     <p>See usage below</p>
     <ul>
-        <li><code>?t=string</code></li>
-        <li><code>?width=number</code></li>
-        <li><code>?scale=number</code></li>
-        <li><code>?margin=number</code></li>
-        <li><code>?colorDark=string</code></li>
-        <li><code>?colorLight=string</code></li>
+        <li><a href="/?t=hello">/?t=hello</a></li>
+        <li><code>/?width=number</code></li>
+        <li><code>/?scale=number</code></li>
+        <li><code>/?margin=number</code></li>
+        <li><code>/?colorDark=string</code></li>
+        <li><code>/?colorLight=string</code></li>
     </ul>
 `;
