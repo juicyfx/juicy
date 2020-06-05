c5b7a7bdab1e38ee8ab975510cbc527d3b511a84
Milan Felix Šulc <rkfelix@gmail.com>
2020-06-05 19:14:30 +0200

    [iconist] generate icon docs

---
+++ packages/iconist/bin/generate.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const app = require('../dist/_lib/app');
+const manager = require('../dist/_lib/manager');
+
+const URL = 'https://iconist-f3l1x.juicyfx1.now.sh';
+
+(async () => {
+
+  for (vendor of app.VENDORS) {
+    const result = await manager.browse({
+      url: URL,
+      vendor,
+    });
+
+    const readme = [`# ${vendor}`, '', ''];
+
+    result.icons.forEach(icon => {
+      readme.push(`- ![](${icon})`);
+    })
+
+    fs.writeFileSync(path.resolve(__dirname, '../docs', `${vendor}.md`), readme.join('\n'));
+  }
+})();
