48ff643262c190f7931015f8371e0d73a55a0d55
Milan Felix Šulc <rkfelix@gmail.com>
2020-06-05 16:43:56 +0200

    [iconist] added ikonate

--- packages/iconist/api/_lib/vendor/ikonate.ts
+++ packages/iconist/api/_lib/vendor/ikonate.ts
@@ -5,7 +5,7 @@ import { readFile } from "../utils";
 export async function generate(req: IconRequest): Promise<string> {
   try {
     // Read icon file
-    var file = await readFile(path.resolve('node_modules', `ikonate/icons/${req.icon}.svg`));
+    var file = await readFile(path.resolve('node_modules', `@obr/ikonate/dist/${req.icon}.svg`));
   } catch (e) {
     throw `Ikonate ${req.icon}.svg not found`;
   }
@@ -20,14 +20,21 @@ export async function generate(req: IconRequest): Promise<string> {
 
   if (req.color) {
     $svg.attr('color', `#${req.color}`);
-    $svg.attr('fill', `#${req.color}`);
+    $svg.attr('stroke', `#${req.color}`);
+  } else {
+    $svg.attr('color', `#000000`);
+    $svg.attr('stroke', `#000000`);
   }
 
+  $svg.attr('fill', 'none');
+  $svg.attr('stroke-width', String((req.stroke || 1) * (24/req.size)));
+  $svg.attr('stroke-linecap', 'round'); // round|square
+  $svg.attr('stroke-linejoin', 'round'); // round|miter
   $svg.attr('width', String(req.size));
   $svg.attr('height', String(req.size));
 
   // Update inner structure
-  // $svg.remove('title');
+  $svg.find('title').remove('title');
 
   // Export icon
   const svg = $.html('svg');
