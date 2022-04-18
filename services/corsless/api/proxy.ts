ece6667d8500d41f8cbd9b7f53b995d3e88c3d05
Milan Felix Šulc <rkfelix@gmail.com>
2022-04-18 20:28:05 +0200

    [corsless] use extended parseUrl

--- services/corsless/api/proxy.ts
+++ services/corsless/api/proxy.ts
@@ -1,6 +1,6 @@
 import http, { IncomingMessage } from "http";
 import https from "https";
-import _url from "url";
+import _url, { UrlWithStringQuery } from "url";
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -15,8 +15,15 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return;
   }
 
+  const parsedUrl = parseUrl(proxyUrl);
+  if (!parsedUrl) {
+    res.statusCode = 400;
+    res.end('Malformed URL');
+    return;
+  }
+
   try {
-    const subres = await proxy(proxyUrl);
+    const subres = await proxy(parsedUrl);
     subres.pipe(res);
   } catch (e: any) {
     console.error(e);
@@ -25,25 +32,59 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 }
 
-function proxy(url: string): Promise<IncomingMessage> {
+function proxy(url: UrlWithStringQuery): Promise<IncomingMessage> {
   return new Promise((resolve, reject) => {
-    const parsed = _url.parse(url);
+    if (url.protocol === null) {
+      url.protocol = 'https:';
+    }
+
 
     const options = {
-      hostname: parsed.hostname,
-      path: parsed.path,
+      hostname: url.hostname,
+      path: url.path,
       headers: { "User-Agent": "Corsless" },
     };
 
-    const api = parsed.protocol === 'https:' ? https : http;
+
+    const api = url.protocol === 'https:' ? https : http;
 
     api
       .get(options, res => {
         resolve(res);
       })
       .on("error", e => {
-        console.error(e);
         reject(e);
       });
   });
+}
+
+function parseUrl(url: string): UrlWithStringQuery | null {
+  const match = url.match(/^(?:(https?:)?\/\/)?(([^\/?]+?)(?::(\d{0,5})(?=[\/?]|$))?)([\/?][\S\s]*|$)/i);
+
+  if (!match) {
+    return null;
+  }
+
+  if (!match[1]) {
+    if (/^https?:/i.test(url)) {
+      // The pattern at top could mistakenly parse "http:///" as host="http:" and path=///.
+      return null;
+    }
+
+    // Scheme is omitted.
+    if (url.lastIndexOf('//', 0) === -1) {
+      // "//" is omitted.
+      url = '//' + url;
+    }
+
+    url = (match[4] === '443' ? 'https:' : 'http:') + url;
+  }
+
+  const parsed = _url.parse(url);
+  if (!parsed.hostname) {
+    // "http://:1/" and "http:/notenoughslashes" could end up here.
+    return null;
+  }
+
+  return parsed;
 }
