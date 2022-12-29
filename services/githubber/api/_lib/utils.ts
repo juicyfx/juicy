fa55d2b2bb959cdaa07fa7fa2cda43e55975863d
Milan Felix Šulc <rkfelix@gmail.com>
2022-12-29 12:50:17 +0100

    [githubber] move to contributte/phpless

--- services/githubber/api/_lib/utils.ts
+++
@@ -1,5 +0,0 @@
-import { NowRequest } from "@vercel/node";
-
-export function queryNumber(req: NowRequest, key: string, defaults: any): number {
-  return parseInt(<string>req.query[key] || defaults);
-}
