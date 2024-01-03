0f86734627b373b247cccd8f9882cd52fe63a33a
Milan Felix Šulc <rkfelix@gmail.com>
2024-01-03 09:52:23 +0100

    [pdfx] switch to sparticuz/chromium

--- services/pdfx/api/_lib/chromium.ts
+++ services/pdfx/api/_lib/chromium.ts
@@ -1,11 +1,11 @@
-import Puppeteer, { Browser } from "puppeteer-core";
-import chromeAws from "chrome-aws-lambda";
+import Puppeteer, { Browser, PuppeteerLaunchOptions } from "puppeteer-core";
+import chromeAws from "@sparticuz/chromium";
 
 export async function createBrowser(args: ChromeLaunchOptions = {}): Promise<Browser> {
   const defaults: ChromeLaunchOptions = {
     ignoreHTTPSErrors: true,
   };
-  let options: ChromeLaunchOptions = {};
+  let options: PuppeteerLaunchOptions = {};
 
   if (isDev()) {
     options = {
@@ -23,7 +23,7 @@ export async function createBrowser(args: ChromeLaunchOptions = {}): Promise<Bro
       ...args,
       ...{
         args: chromeAws.args,
-        executablePath: await chromeAws.executablePath,
+        executablePath: await chromeAws.executablePath(),
         headless: chromeAws.headless,
       }
     };
