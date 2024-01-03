342fd9cf0314f763a83a297f00471749e967691b
Milan Felix Šulc <rkfelix@gmail.com>
2024-01-03 09:52:09 +0100

    [fcbk] switch to sparticuz/chromium

--- services/fcbk/api/_lib/chromium.ts
+++ services/fcbk/api/_lib/chromium.ts
@@ -1,5 +1,5 @@
-import Puppeteer, { Browser } from "puppeteer-core";
-import chromeAws from "chrome-aws-lambda";
+import Puppeteer, { Browser, PuppeteerLaunchOptions } from "puppeteer-core";
+import chromeAws from "@sparticuz/chromium";
 
 export async function createBrowser(args: ChromeLaunchOptions = {}): Promise<Browser> {
   const defaults: ChromeLaunchOptions = {
@@ -10,7 +10,7 @@ export async function createBrowser(args: ChromeLaunchOptions = {}): Promise<Bro
     },
     ignoreHTTPSErrors: true,
   };
-  let options: ChromeLaunchOptions = {};
+  let options: PuppeteerLaunchOptions = {};
 
   if (isDev()) {
     options = {
@@ -28,7 +28,7 @@ export async function createBrowser(args: ChromeLaunchOptions = {}): Promise<Bro
       ...args,
       ...{
         args: chromeAws.args,
-        executablePath: await chromeAws.executablePath,
+        executablePath: await chromeAws.executablePath(),
         headless: chromeAws.headless,
       }
     };
